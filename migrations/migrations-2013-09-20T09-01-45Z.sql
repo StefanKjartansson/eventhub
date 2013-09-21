f6352f25452054d8b4cd555724c06c0d9cf59d81
Stefan Kjartansson <esteban.supreme@gmail.com>
2013-09-21 15:46:46 +0000

    removed old code

--- migrations/migrations-2013-09-20T09-01-45Z.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE migration_info (
-    id serial NOT NULL PRIMARY KEY,
-    created timestamp with time zone NOT NULL,
-    content text NOT NULL 
-);
--- migrations/new-migration.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-touch "migrations-"$(date -u +"%Y-%m-%dT%H-%M-%SZ")".sql"
