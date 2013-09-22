f1063749a70369ad5745ce56ddf9f176f6dabea9
Stefan Kjartansson <esteban.supreme@gmail.com>
2013-09-22 23:14:45 +0000

    Added filter by

--- interfaces.go
+++ interfaces.go
@@ -3,6 +3,11 @@ package eventhub
 type DataBackend interface {
 	Save(e *Event) error
 	GetById(id int) (*Event, error)
-	//FilterByKey(key string) (*[]Event, error)
+	FilterBy(m map[string]interface{}) ([]*Event, error)
 }
 
+type EventFeed interface {
+	Updates() <-chan Event
+	Close() error
+	loop() error
+}
