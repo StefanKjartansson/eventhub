8e3ce9bd5f8000def94c1d1bc3476bd560e2dfc0
Stefan Kjartansson <esteban.supreme@gmail.com>
2013-09-22 23:15:07 +0000

    update and filter by test

--- test_utils.go
+++ test_utils.go
@@ -1,7 +1,7 @@
 package eventhub
 
 import (
-    "testing"
+	"testing"
 )
 
 func RunDataBackendTest(t *testing.T, d DataBackend) {
@@ -22,7 +22,11 @@ func RunDataBackendTest(t *testing.T, d DataBackend) {
 		Actors:      []string{"someone"},
 	}
 
-	d.Save(&e)
+	err := d.Save(&e)
+	if err != nil {
+		t.Error("PostgresDataSource has error:", err)
+		return
+	}
 
 	if e.ID != 1 {
 		t.Errorf("Expected '%d', got %v", 1, e.ID)
@@ -36,8 +40,8 @@ func RunDataBackendTest(t *testing.T, d DataBackend) {
 
 	t.Logf("%v", newE)
 
-    newE.Description = "New Description"
-    err = d.Save(newE)
+	newE.Description = "New Description"
+	err = d.Save(newE)
 	if err != nil {
 		t.Error("PostgresDataSource has error:", err)
 		return
@@ -46,6 +50,43 @@ func RunDataBackendTest(t *testing.T, d DataBackend) {
 	updated, err := d.GetById(1)
 	if updated.Description != newE.Description {
 		t.Error("PostgresDataSource has error:", err)
+		return
+	}
+
+	err = d.Save(&Event{
+		Key:         "foo.baz",
+		Payload:     data,
+		Description: "My event 2",
+		Importance:  3,
+		Origin:      "mysystem",
+		Entities:    []string{"ns/foo", "ns/moo"},
+		Actors:      []string{"someone"},
+	})
+	if err != nil {
+		t.Error("PostgresDataSource has error:", err)
+		return
+	}
+
+	m := make(map[string]interface{})
+	//m["Key"] = "foo.*"
+	m["Origin"] = "mysystem"
+
+	evs, err := d.FilterBy(m)
+
+	t.Log(evs)
+
+	if err != nil {
+		t.Error("PostgresDataSource has error:", err)
+		return
+	}
+
+	if len(evs) != 2 {
+		t.Error("Filter results count should've been 2")
+		return
+	}
+
+	if evs[0].Origin != "mysystem" {
+		t.Error("Origin not expected")
 		return
 	}
 }
