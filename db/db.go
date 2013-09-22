ab369ad07ba3ec036f2962e7b40e362fd2d127b5
Stefan Kjartansson <esteban.supreme@gmail.com>
2013-09-22 23:14:04 +0000

    moved the queries into a separate file

--- db/db.go
+++ db/db.go
@@ -5,83 +5,7 @@ import (
 	"encoding/json"
 	"github.com/StefanKjartansson/eventhub"
 	_ "github.com/lib/pq"
-	"log"
 	"strings"
-)
-
-const (
-	insertSQL = `
-    INSERT INTO "event"
-        (
-            "key",
-            "created",
-            "updated",
-            "payload",
-            "description",
-            "importance",
-            "origin",
-            "entities",
-            "other_references",
-            "actors",
-            "tags"
-        )
-    VALUES
-        (
-            $1,
-            now(),
-            now(),
-            $2,
-            $3,
-            $4,
-            $5,
-            ARRAY[$6],
-            ARRAY[$7],
-            ARRAY[$8],
-            ARRAY[$9]
-        )
-    RETURNING
-        "id",
-        "created",
-        "updated";
-    `
-
-	updateSQL = `
-    UPDATE "event"
-    SET
-        "key" = $1,
-        "payload" = $2,
-        "description" = $3,
-        "importance" = $4,
-        "origin" = $5,
-        "entities" = ARRAY[$6],
-        "other_references" = ARRAY[$7],
-        "actors" = ARRAY[$8],
-        "tags" = ARRAY[$9],
-        "updated" = now()
-    WHERE
-        "id" = $10
-    RETURNING
-        "updated";
-    `
-
-	selectByIdSQL = `
-    SELECT
-        "id",
-        "key",
-        "created",
-        "updated",
-        "payload",
-        "description",
-        "importance",
-        "origin",
-        "entities",
-        "other_references",
-        "actors",
-        "tags"
-    FROM
-        "event"
-    WHERE "id" = $1
-    `
 )
 
 type PostgresDataSource struct {
@@ -91,33 +15,15 @@ type PostgresDataSource struct {
 	selectbyid *sql.Stmt
 }
 
-func (p *PostgresDataSource) GetById(id int) (*eventhub.Event, error) {
-
-	var e eventhub.Event
+func scanRow(row *sql.Rows, e *eventhub.Event) error {
 	var err error
-	var tx *sql.Tx
-
-	if tx, err = p.pg.Begin(); err != nil {
-		return nil, err
-	}
-
-	defer func() {
-		if err != nil {
-			tx.Rollback()
-			log.Fatal(err)
-		} else {
-			tx.Commit()
-		}
-	}()
-
 	var entities StringSlice
 	var references StringSlice
 	var actors StringSlice
 	var tags StringSlice
-
 	temp := []byte{}
 
-	err = tx.Stmt(p.selectbyid).QueryRow(id).Scan(
+	err = row.Scan(
 		&e.ID,
 		&e.Key,
 		&e.Created,
@@ -132,14 +38,14 @@ func (p *PostgresDataSource) GetById(id int) (*eventhub.Event, error) {
 		&tags)
 
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	var data interface{}
 	err = json.Unmarshal(temp, &data)
 
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	e.Payload = data
@@ -148,26 +54,35 @@ func (p *PostgresDataSource) GetById(id int) (*eventhub.Event, error) {
 	e.Actors = actors
 	e.Tags = tags
 
+	return nil
+
+}
+
+func (p *PostgresDataSource) GetById(id int) (*eventhub.Event, error) {
+
+	var e eventhub.Event
+	var err error
+
+	err = wrapTransaction(p.pg, func(tx *sql.Tx) error {
+		rows, err := tx.Stmt(p.selectbyid).Query(id)
+		if err != nil {
+			return err
+		}
+		defer rows.Close()
+		if !rows.Next() {
+			return sql.ErrNoRows
+		}
+		return scanRow(rows, &e)
+	})
+
+	if err != nil {
+		return nil, err
+	}
 	return &e, nil
 
 }
 
 func (p *PostgresDataSource) Save(e *eventhub.Event) (err error) {
-
-	var tx *sql.Tx
-
-	if tx, err = p.pg.Begin(); err != nil {
-		return
-	}
-
-	defer func() {
-		if err != nil {
-			tx.Rollback()
-			log.Fatal(err)
-		} else {
-			tx.Commit()
-		}
-	}()
 
 	b, err := json.Marshal(e.Payload)
 	if err != nil {
@@ -188,17 +103,17 @@ func (p *PostgresDataSource) Save(e *eventhub.Event) (err error) {
 
 	switch e.ID {
 	case 0:
-		err = tx.Stmt(p.insert).QueryRow(args...).Scan(&e.ID, &e.Created, &e.Updated)
+		err = wrapTransaction(p.pg, func(tx *sql.Tx) error {
+			return tx.Stmt(p.insert).QueryRow(args...).Scan(&e.ID, &e.Created, &e.Updated)
+		})
 	default:
 		args := append(args, e.ID)
-		err = tx.Stmt(p.update).QueryRow(args...).Scan(&e.Updated)
+		err = wrapTransaction(p.pg, func(tx *sql.Tx) error {
+			return tx.Stmt(p.update).QueryRow(args...).Scan(&e.Updated)
+		})
 	}
 
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return err
 }
 
 func NewPostgresDataSource(connection string) (*PostgresDataSource, error) {
