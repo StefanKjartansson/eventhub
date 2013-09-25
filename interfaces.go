0e9f720b4636a88b092029c70f36dc409e5eb939
Stefan Kjartansson <esteban.supreme@gmail.com>
2013-09-25 14:30:44 +0000

    data backend now needs to implement eventfeed

--- interfaces.go
+++ interfaces.go
@@ -1,14 +1,16 @@
 package eventhub
 
-type DataBackend interface {
-	Save(e *Event) error
-	GetById(id int) (*Event, error)
-	FilterBy(m map[string]interface{}) ([]*Event, error)
-}
-
 type EventFeed interface {
 	Updates() <-chan Event
 	Close() error
+}
+
+//Queryable Data store
+type DataBackend interface {
+	EventFeed
+	Save(e *Event) error
+	GetById(id int) (*Event, error)
+	FilterBy(m map[string]interface{}) ([]*Event, error)
 }
 
 type Broadcaster interface {
